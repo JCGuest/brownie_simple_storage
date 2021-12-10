67ce017c66acb5969fbc8437b6169d22c7905913
JCGuest <jcguest90@gmail.com>
2021-12-09 21:03:20 -0600

    add test

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,10 +1,16 @@
-from brownie import accounts, config, accounts
+from brownie import accounts, config, SimpleStorage
 
 
 def deploy_simple_storage():
-    # account = accounts.load("second")
-    account = accounts.add(config["wallets"]["from_key"])
-    print(account)
+    account = accounts[0]
+    # account = accounts.add(config["wallets"]["from_key"])
+    simple_storage = SimpleStorage.deploy({"from": account})
+    stored_value = simple_storage.retrieve()
+    print(f"Stored value: {stored_value}")
+    transaction = simple_storage.store(42, {"from": account})
+    transaction.wait(1)
+    new_value = simple_storage.retrieve()
+    print(f"New value: {new_value}")
 
 
 def main():
